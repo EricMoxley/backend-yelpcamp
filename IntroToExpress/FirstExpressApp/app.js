58edba8a4cba067ff06aa071002904d304e8160e
EricMoxley <exilerzero@yahoo.com>
2019-09-30 02:45:51 -0400

    First express app created
    Showcasing creating port 3000 server as well as get/res console.logs

---
+++ IntroToExpress/FirstExpressApp/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+// "/" => "Hello!"
+app.get("/", function(req, res){
+    console.log('Someone requested this page!');
+    res.send("Hello!");
+});
+
+// "/bye" => "Goodbye!"
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+
+// "/pog" => "champ!"
+app.get("/pog", function(req, res){
+    res.send("champ!");
+});
+
+app.listen(3000, function() { 
+    console.log('Server listening on port 3000'); 
+  });
--- MyShop/listProducts.js
+++ MyShop/listProducts.js
@@ -5,3 +5,5 @@ let randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
 let randomCard = faker.helpers.createCard(); // random contact card containing many properties
 
 console.log(randomName);
+console.log(randomEmail);
+console.log(randomCard);
