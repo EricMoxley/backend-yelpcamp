e0742f3cb865da6596cff1a2287d5d5020fa13a7
EricMoxley <exilerzero@yahoo.com>
2019-10-21 04:11:58 -0400

    create restfulroutingapp

--- RestfulRouting/RESTfulBlogApp/app.js
+++ RestfulRouting/RESTfulBlogApp/app.js
@@ -4,10 +4,12 @@ express        = require("express"),
 app            = express();
 
 //APP Config
-mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res){
+    res.render("landing");
+});
 
 // Mongoose/Model Config
 var blogSchema = new mongoose.Schema({
@@ -17,6 +19,12 @@ var blogSchema = new mongoose.Schema({
     created: {type: Date, default: Date.now}
 });
 var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+    title: "Test Blog",
+    image: "https://unsplash.com/photos/KdeqA3aTnBY",
+    body: "Hello this is a blog post!"
+});
 
 // Restful Routes
 
